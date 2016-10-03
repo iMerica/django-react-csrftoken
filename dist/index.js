bf6027e5bad550178bd41043278c379514e8b78f
Michael <michaelmartinez@me.com>
2016-10-03 01:05:56 -0700

    Updates index

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,64 @@
-import React from 'react';
-import cookie from 'react-cookie';
+'use strict';
 
-class DjangoCSRFToken extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      csrfToken: cookie.load('csrftoken')
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _getPrototypeOf = require('babel-runtime/core-js/object/get-prototype-of');
+
+var _getPrototypeOf2 = _interopRequireDefault(_getPrototypeOf);
+
+var _classCallCheck2 = require('babel-runtime/helpers/classCallCheck');
+
+var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
+
+var _createClass2 = require('babel-runtime/helpers/createClass');
+
+var _createClass3 = _interopRequireDefault(_createClass2);
+
+var _possibleConstructorReturn2 = require('babel-runtime/helpers/possibleConstructorReturn');
+
+var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
+
+var _inherits2 = require('babel-runtime/helpers/inherits');
+
+var _inherits3 = _interopRequireDefault(_inherits2);
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactCookie = require('react-cookie');
+
+var _reactCookie2 = _interopRequireDefault(_reactCookie);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var DjangoCSRFToken = function (_React$Component) {
+  (0, _inherits3.default)(DjangoCSRFToken, _React$Component);
+
+  function DjangoCSRFToken() {
+    (0, _classCallCheck3.default)(this, DjangoCSRFToken);
+
+    var _this = (0, _possibleConstructorReturn3.default)(this, (DjangoCSRFToken.__proto__ || (0, _getPrototypeOf2.default)(DjangoCSRFToken)).call(this));
+
+    _this.state = {
+      csrfToken: _reactCookie2.default.load('csrftoken')
     };
+    return _this;
   }
-  render() {
-    return React.DOM.input({
-      type: 'hidden',
-      name: 'csrfmiddlewaretoken',
-      value: this.state.csrfToken
-    });
-  }
-}
 
-export default DjangoCSRFToken;
+  (0, _createClass3.default)(DjangoCSRFToken, [{
+    key: 'render',
+    value: function render() {
+      return _react2.default.DOM.input({
+        type: 'hidden',
+        name: 'csrfmiddlewaretoken',
+        value: this.state.csrfToken
+      });
+    }
+  }]);
+  return DjangoCSRFToken;
+}(_react2.default.Component);
+
+exports.default = DjangoCSRFToken;
